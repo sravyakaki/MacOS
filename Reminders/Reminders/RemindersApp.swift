a84b57d0395444255895a9a28888f84e498afaa4
luv_dev <sravyakaki@users.noreply.github.com>
2025-05-13 16:40:49 -0700

    Default App Shell

---
+++ Reminders/Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Sravya Kaki on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
